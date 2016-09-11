326fe4106b450f0321f9183955ca1926179391fc
Aaron L. Zeng <a2z@mit.edu>
2016-09-11 03:36:13 -0400

    Display error status when it occurs

--- public/js/prime-checker.js
+++ public/js/prime-checker.js
@@ -2,9 +2,13 @@ $(function() {
     $('#n').keyup(function() {
         var n = $(this).val();
         if (n !== '') {
-            $.getJSON('/is_prime/' + n, function(data) {
-                $('#result').text(data ? 'Prime' : 'Composite');
-            });
+            $.getJSON('/is_prime/' + n)
+                .done(function(data) {
+                    $('#result').text(data ? 'Prime' : 'Composite');
+                })
+                .fail(function() {
+                    $('#result').text('Error');
+                });
         } else {
             $('#result').text('');
         }
