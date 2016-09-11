096238542a0c5f509cd6070983cb7ae3fc593f6b
Aaron L. Zeng <a2z@mit.edu>
2016-09-11 03:17:31 -0400

    Display Prime/Composite instead of true/false

--- public/js/prime-checker.js
+++ public/js/prime-checker.js
@@ -3,7 +3,7 @@ $(function() {
         var n = $(this).val();
         if (n !== '') {
             $.getJSON('/is_prime/' + n, function(data) {
-                $('#result').text(data);
+                $('#result').text(data ? 'Prime' : 'Composite');
             });
         } else {
             $('#result').text('');
