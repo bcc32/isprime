6bc1641a010b21e147028c564cee3968d5470f3f
Aaron L. Zeng <a2z@mit.edu>
2017-01-28 00:43:18 -0500

    Cache API calls and fix race condition
    Race condition occurs if slow GET returns after the text has changed.

--- public/js/prime-checker.js
+++ public/js/prime-checker.js
@@ -1,19 +1,38 @@
 $(function() {
+    var cache = {};
+
+    function display(data) {
+        if (typeof data === 'object') {
+            return 'Error: ' + data.error;
+        }
+        var isPrime = data;
+        return isPrime ? 'Prime' : 'Not Prime';
+    }
+
+    function repaint() {
+        var n = $('#n').val();
+        var $result = $('#result');
+        if (n === '') return $result.text('');
+        if (cache[n] != null) return $result.text(display(cache[n]));
+        $result.text('...');
+    }
+
     $('#n').keyup(function() {
         var n = $(this).val();
-        if (n !== '') {
-            $.getJSON('/is_prime/' + n)
-                .done(function(data) {
-                    $('#result').text(data ? 'Prime' : 'Composite');
-                })
-                .fail(function(jqXHR, textStatus, errorThrown) {
-                    var data = JSON.parse(jqXHR.responseText);
-                    $('#result').text('Error: ' + data.error);
-                });
-        } else {
-            $('#result').text('');
-        }
+        if (n === '') return repaint();
+        if (cache[n] != null) return repaint();
+        $('#result').text('...');
+        $.getJSON('/is_prime/' + n)
+            .done(function(data) {
+                cache[n] = data;
+            })
+            .fail(function(jqXHR, textStatus, errorThrown) {
+                var data = JSON.parse(jqXHR.responseText);
+                cache[n] = data;
+            })
+            .always(repaint);
     });
+
     $(document).keyup(function(e) {
         if (e.keyCode === 81) { // q
             $('#n').focus();
