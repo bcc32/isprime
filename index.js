684bdc3194e31766d814273ebadd59c060d646bb
Aaron L. Zeng <a2z@mit.edu>
2017-01-28 01:15:11 -0500

    Lint

--- index.js
+++ index.js
@@ -12,18 +12,18 @@ app.set('views', './views');
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
+  res.render('index');
 });
 
 app.get('/is_prime/:n', (req, res) => {
-    var n = +req.params.n;
-    if (0 <= n && n <= Number.MAX_SAFE_INTEGER) {
-        res.send(isPrime(n));
-    } else if (Number.isNaN(n)) {
-        res.status(400).json({error: 'not a number', n: req.params.n});
-    } else {
-        res.status(400).json({error: 'out of range', n: req.params.n});
-    }
+  var n = +req.params.n;
+  if (0 <= n && n <= Number.MAX_SAFE_INTEGER) {
+    res.send(isPrime(n));
+  } else if (Number.isNaN(n)) {
+    res.status(400).json({error: 'not a number', n: req.params.n});
+  } else {
+    res.status(400).json({error: 'out of range', n: req.params.n});
+  }
 });
 
 app.use(express.static('public'));
--- public/js/prime-checker.js
+++ public/js/prime-checker.js
@@ -1,43 +1,43 @@
 $(function() {
-    var cache = {};
+  var cache = {};
 
-    function display(data) {
-        if (typeof data === 'object') {
-            return 'Error: ' + data.error;
-        }
-        var isPrime = data;
-        return isPrime ? 'Prime' : 'Not Prime';
+  function display(data) {
+    if (typeof data === 'object') {
+      return 'Error: ' + data.error;
     }
+    var isPrime = data;
+    return isPrime ? 'Prime' : 'Not Prime';
+  }
 
-    function repaint() {
-        var n = $('#n').val();
-        var $result = $('#result');
-        if (n === '') return $result.text('');
-        if (cache[n] != null) return $result.text(display(cache[n]));
-        $result.text('...');
-    }
+  function repaint() {
+    var n = $('#n').val();
+    var $result = $('#result');
+    if (n === '') return $result.text('');
+    if (cache[n] != null) return $result.text(display(cache[n]));
+    $result.text('...');
+  }
 
-    $('#n').keyup(function() {
-        var n = $(this).val();
-        if (n === '') return repaint();
-        if (cache[n] != null) return repaint();
-        $('#result').text('...');
-        $.getJSON('/is_prime/' + n)
+  $('#n').keyup(function() {
+    var n = $(this).val();
+    if (n === '') return repaint();
+    if (cache[n] != null) return repaint();
+    $('#result').text('...');
+    $.getJSON('/is_prime/' + n)
             .done(function(data) {
-                cache[n] = data;
+              cache[n] = data;
             })
-            .fail(function(jqXHR, textStatus, errorThrown) {
-                var data = JSON.parse(jqXHR.responseText);
-                cache[n] = data;
+            .fail(function(jqXHR) {
+              var data = JSON.parse(jqXHR.responseText);
+              cache[n] = data;
             })
             .always(repaint);
-    });
+  });
 
-    $(document).keyup(function(e) {
-        if (e.keyCode === 81) { // q
-            $('#n').focus();
-        } else if (e.keyCode === 27) { // escape
-            $('#n').blur().val('');
-        }
-    });
+  $(document).keyup(function(e) {
+    if (e.keyCode === 81) { // q
+      $('#n').focus();
+    } else if (e.keyCode === 27) { // escape
+      $('#n').blur().val('');
+    }
+  });
 });
